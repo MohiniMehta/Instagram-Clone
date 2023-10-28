65c29bccc2b835e45ee22a44542796e69f972ff7
MohiniMehta <mohini.mehta21@pccoepune.org>
2023-10-28 23:17:48 +0530

    Insta-clone using flutter

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const Scaffold(
+        body: Text('Lets build instagram clone'),
+      ),
+    );
+  }
+}
