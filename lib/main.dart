d9ae279f772dbf2103c47c7f5385e1fc64d7176e
MohiniMehta <mohini.mehta21@pccoepune.org>
2023-11-12 22:39:11 +0530

    Persiting Auth State

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/screens/login_screen.dart';
 //import 'package:instagram_clone/screens/signup_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
@@ -37,8 +41,32 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      //home: const ResponsiveLayout(webScreenLayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout()),
-      home: const LoginScreen(),
+      //home: 
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+         builder: (context,snapshot){
+          if(snapshot.connectionState==ConnectionState.active){
+            if(snapshot.hasData){
+              return const ResponsiveLayout(
+                webScreenLayout: WebScreenLayout(),
+                 mobileScreenLayout: MobileScreenLayout()
+                 );
+            }
+            else if(snapshot.hasError){
+              return Center(
+                child: Text('$(snapshot.error)'),);
+            }
+          }
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(
+                color: primaryColor,
+              ),
+            );
+          }
+          return const LoginScreen();
+         },
+         ),
     );
   }
 }
